a685d30b6166b35597a502a1ac9c87190d859280
Anas-Ouardi <anasouardi101@gmail.com>
2023-05-17 20:59:11 +0100

    New staf

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const Port = process.env.Port || 5000
 
 const app = express()
 
+app.get('/dev',(req,res)=>{
+    res.status(200).json({message:"get dev"});
+})
+
 app.listen(Port,()=>console.log(`The server is runing on Port : ${Port}`));
