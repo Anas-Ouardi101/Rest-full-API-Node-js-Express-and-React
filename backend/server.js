0b610cd12177151d025e13a61ecf7733e7367f27
Anas-Ouardi <anasouardi101@gmail.com>
2023-05-17 20:08:24 +0100

    The first server change

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('the server is runing');
+const express = require("express");
+const dotenv = require("dotenv").config()
+const Port = process.env.Port || 5000
+
+const app = express()
+
+app.listen(Port,()=>console.log(`The server is runing on Port : ${Port}`));
